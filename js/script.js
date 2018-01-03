70e9a9f575a129f9185d99ac1faeeedf9dd1dfb9
Stefani Urmas <32307202+StefUrmas@users.noreply.github.com>
2018-01-03 15:01:29 -0800

    readme
    Gentrification stories added.

--- js/script.js
+++ js/script.js
@@ -242,6 +242,421 @@ $(document).ready(function(){
     }
     });
     
+    // AUDIO PROGRESS BAR SOUND 5
+
+    var myAudio5 = document.getElementById('player-05'); // get the audio data from the image-audio box
+    console.log(myAudio5)
+    myAudio4.addEventListener('progress5', function() {
+    var duration5 =  myAudio5.duration;
+    if (duration5 > 0) {
+      for (var i = 0; i < myAudio4.buffered5.length; i++) {
+            if (myAudio5.buffered5.start(myAudio5.buffered5.length - 1 - i) < myAudio5.currentTime) {
+                document.getElementById("buffered-amount5").style.width = (myAudio5.buffered5.end(myAudio5.buffered5.length - 1 - i) / duration5) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio5.addEventListener('timeupdate', function() {
+    var duration5 =  myAudio5.duration;
+    if (duration5 > 0) {
+      document.getElementById('progress-amount5').style.width = ((myAudio5.currentTime / duration5)*100) + "%";
+    }
+    });  
+        
+    // AUDIO PROGRESS BAR SOUND 6
+
+    var myAudio6 = document.getElementById('player-06'); // get the audio data from the image-audio box
+    console.log(myAudio6)
+    myAudio6.addEventListener('progress6', function() {
+    var duration6 =  myAudio6.duration;
+    if (duration6 > 0) {
+      for (var i = 0; i < myAudio6.buffered6.length; i++) {
+            if (myAudio6.buffered6.start(myAudio6.buffered6.length - 1 - i) < myAudio6.currentTime) {
+                document.getElementById("buffered-amount6").style.width = (myAudio6.buffered6.end(myAudio6.buffered6.length - 1 - i) / duration6) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio6.addEventListener('timeupdate', function() {
+    var duration6 =  myAudio6.duration;
+    if (duration6 > 0) {
+      document.getElementById('progress-amount6').style.width = ((myAudio6.currentTime / duration6)*100) + "%";
+    }
+    });   
+    
+        
+    // AUDIO PROGRESS BAR SOUND 7
+
+    var myAudio7 = document.getElementById('player-07'); // get the audio data from the image-audio box
+    console.log(myAudio7)
+    myAudio7.addEventListener('progress7', function() {
+    var duration7 =  myAudio7.duration;
+    if (duration7 > 0) {
+      for (var i = 0; i < myAudio7.buffered7.length; i++) {
+            if (myAudio7.buffered7.start(myAudio7.buffered7.length - 1 - i) < myAudio7.currentTime) {
+                document.getElementById("buffered-amount7").style.width = (myAudio7.buffered7.end(myAudio7.buffered7.length - 1 - i) / duration7) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio7.addEventListener('timeupdate', function() {
+    var duration7 =  myAudio7.duration;
+    if (duration7 > 0) {
+      document.getElementById('progress-amount7').style.width = ((myAudio7.currentTime / duration7)*100) + "%";
+    }
+    }); 
+        
+    // AUDIO PROGRESS BAR SOUND 8
+
+    var myAudio8 = document.getElementById('player-08'); // get the audio data from the image-audio box
+    console.log(myAudio8)
+    myAudio8.addEventListener('progress8', function() {
+    var duration8 =  myAudio8.duration;
+    if (duration8 > 0) {
+      for (var i = 0; i < myAudio8.buffered8.length; i++) {
+            if (myAudio8.buffered8.start(myAudio8.buffered8.length - 1 - i) < myAudio8.currentTime) {
+                document.getElementById("buffered-amount8").style.width = (myAudio8.buffered8.end(myAudio8.buffered8.length - 1 - i) / duration8) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio8.addEventListener('timeupdate', function() {
+    var duration8 =  myAudio8.duration;
+    if (duration8 > 0) {
+      document.getElementById('progress-amount8').style.width = ((myAudio8.currentTime / duration8)*100) + "%";
+    }
+    });
+        
+    // AUDIO PROGRESS BAR SOUND 9
+
+    var myAudio9 = document.getElementById('player-09'); // get the audio data from the image-audio box
+    console.log(myAudio9)
+    myAudio9.addEventListener('progress9', function() {
+    var duration9 =  myAudio9.duration;
+    if (duration9 > 0) {
+      for (var i = 0; i < myAudio9.buffered9.length; i++) {
+            if (myAudio9.buffered9.start(myAudio9.buffered9.length - 1 - i) < myAudio9.currentTime) {
+                document.getElementById("buffered-amount9").style.width = (myAudio9.buffered9.end(myAudio9.buffered9.length - 1 - i) / duration9) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio9.addEventListener('timeupdate', function() {
+    var duration9 =  myAudio9.duration;
+    if (duration9 > 0) {
+      document.getElementById('progress-amount9').style.width = ((myAudio9.currentTime / duration9)*100) + "%";
+    }
+    });
+        
+    // AUDIO PROGRESS BAR SOUND 10
+
+    var myAudio10 = document.getElementById('player-10'); // get the audio data from the image-audio box
+    console.log(myAudio10)
+    myAudio10.addEventListener('progress10', function() {
+    var duration10 =  myAudio10.duration;
+    if (duration10 > 0) {
+      for (var i = 0; i < myAudio10.buffered10.length; i++) {
+            if (myAudio10.buffered10.start(myAudio10.buffered10.length - 1 - i) < myAudio10.currentTime) {
+                document.getElementById("buffered-amount10").style.width = (myAudio10.buffered10.end(myAudio10.buffered10.length - 1 - i) / duration10) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio10.addEventListener('timeupdate', function() {
+    var duration10 =  myAudio10.duration;
+    if (duration10 > 0) {
+      document.getElementById('progress-amount10').style.width = ((myAudio10.currentTime / duration10)*100) + "%";
+    }
+    });
+        
+    // AUDIO PROGRESS BAR SOUND 11
+
+    var myAudio11 = document.getElementById('player-11'); // get the audio data from the image-audio box
+    console.log(myAudio11)
+    myAudio11.addEventListener('progress11', function() {
+    var duration11 =  myAudio11.duration;
+    if (duration11 > 0) {
+      for (var i = 0; i < myAudio11.buffered11.length; i++) {
+            if (myAudio11.buffered11.start(myAudio11.buffered11.length - 1 - i) < myAudio11.currentTime) {
+                document.getElementById("buffered-amount11").style.width = (myAudio11.buffered11.end(myAudio11.buffered11.length - 1 - i) / duration11) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio11.addEventListener('timeupdate', function() {
+    var duration11 =  myAudio11.duration;
+    if (duration11 > 0) {
+      document.getElementById('progress-amount11').style.width = ((myAudio11.currentTime / duration11)*100) + "%";
+    }
+    }); 
+        
+    // AUDIO PROGRESS BAR SOUND 12
+
+    var myAudio12 = document.getElementById('player-12'); // get the audio data from the image-audio box
+    console.log(myAudio12)
+    myAudio12.addEventListener('progress12', function() {
+    var duration12 =  myAudio12.duration;
+    if (duration12 > 0) {
+      for (var i = 0; i < myAudio12.buffered12.length; i++) {
+            if (myAudio12.buffered12.start(myAudio12.buffered12.length - 1 - i) < myAudio12.currentTime) {
+                document.getElementById("buffered-amount12").style.width = (myAudio12.buffered12.end(myAudio12.buffered12.length - 1 - i) / duration12) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio12.addEventListener('timeupdate', function() {
+    var duration12 =  myAudio12.duration;
+    if (duration12 > 0) {
+      document.getElementById('progress-amount12').style.width = ((myAudio12.currentTime / duration12)*100) + "%";
+    }
+    });  
+        
+    // AUDIO PROGRESS BAR SOUND 13
+
+    var myAudio13 = document.getElementById('player-13'); // get the audio data from the image-audio box
+    console.log(myAudio13)
+    myAudio13.addEventListener('progress13', function() {
+    var duration13 =  myAudio13.duration;
+    if (duration13 > 0) {
+      for (var i = 0; i < myAudio13.buffered13.length; i++) {
+            if (myAudio13.buffered13.start(myAudio13.buffered13.length - 1 - i) < myAudio13.currentTime) {
+                document.getElementById("buffered-amount13").style.width = (myAudio13.buffered13.end(myAudio13.buffered13.length - 1 - i) / duration13) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio13.addEventListener('timeupdate', function() {
+    var duration13 =  myAudio13.duration;
+    if (duration13 > 0) {
+      document.getElementById('progress-amount13').style.width = ((myAudio13.currentTime / duration13)*100) + "%";
+    }
+    });  
+        
+        // AUDIO PROGRESS BAR SOUND 14
+
+    var myAudio14 = document.getElementById('player-14'); // get the audio data from the image-audio box
+    console.log(myAudio14)
+    myAudio14.addEventListener('progress14', function() {
+    var duration14 =  myAudio14.duration;
+    if (duration14 > 0) {
+      for (var i = 0; i < myAudio14.buffered14.length; i++) {
+            if (myAudio14.buffered14.start(myAudio14.buffered14.length - 1 - i) < myAudio14.currentTime) {
+                document.getElementById("buffered-amount14").style.width = (myAudio14.buffered14.end(myAudio14.buffered14.length - 1 - i) / duration14) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio14.addEventListener('timeupdate', function() {
+    var duration14 =  myAudio14.duration;
+    if (duration14 > 0) {
+      document.getElementById('progress-amount14').style.width = ((myAudio14.currentTime / duration14)*100) + "%";
+    }
+    });    
+        
+    // AUDIO PROGRESS BAR SOUND 15
+
+    var myAudio15 = document.getElementById('player-15'); // get the audio data from the image-audio box
+    console.log(myAudio15)
+    myAudio15.addEventListener('progress15', function() {
+    var duration15 =  myAudio15.duration;
+    if (duration15 > 0) {
+      for (var i = 0; i < myAudio15.buffered15.length; i++) {
+            if (myAudio15.buffered15.start(myAudio15.buffered15.length - 1 - i) < myAudio15.currentTime) {
+                document.getElementById("buffered-amount15").style.width = (myAudio15.buffered15.end(myAudio15.buffered15.length - 1 - i) / duration15) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio15.addEventListener('timeupdate', function() {
+    var duration15 =  myAudio15.duration;
+    if (duration15 > 0) {
+      document.getElementById('progress-amount15').style.width = ((myAudio15.currentTime / duration15)*100) + "%";
+    }
+    });
+        
+    // AUDIO PROGRESS BAR SOUND 16
+
+    var myAudio16 = document.getElementById('player-16'); // get the audio data from the image-audio box
+    console.log(myAudio16)
+    myAudio16.addEventListener('progress16', function() {
+    var duration16 =  myAudio16.duration;
+    if (duration16 > 0) {
+      for (var i = 0; i < myAudio16.buffered16.length; i++) {
+            if (myAudio16.buffered16.start(myAudio16.buffered16.length - 1 - i) < myAudio16.currentTime) {
+                document.getElementById("buffered-amount16").style.width = (myAudio16.buffered16.end(myAudio16.buffered16.length - 1 - i) / duration16) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio16.addEventListener('timeupdate', function() {
+    var duration16 =  myAudio16.duration;
+    if (duration16 > 0) {
+      document.getElementById('progress-amount16').style.width = ((myAudio16.currentTime / duration16)*100) + "%";
+    }
+    });  
+        
+        // AUDIO PROGRESS BAR SOUND 17
+
+    var myAudio17 = document.getElementById('player-17'); // get the audio data from the image-audio box
+    console.log(myAudio17)
+    myAudio17.addEventListener('progress17', function() {
+    var duration17 =  myAudio17.duration;
+    if (duration17 > 0) {
+      for (var i = 0; i < myAudio17.buffered17.length; i++) {
+            if (myAudio17.buffered17.start(myAudio17.buffered17.length - 1 - i) < myAudio17.currentTime) {
+                document.getElementById("buffered-amount17").style.width = (myAudio17.buffered17.end(myAudio17.buffered17.length - 1 - i) / duration17) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio17.addEventListener('timeupdate', function() {
+    var duration17 =  myAudio17.duration;
+    if (duration17 > 0) {
+      document.getElementById('progress-amount17').style.width = ((myAudio17.currentTime / duration17)*100) + "%";
+    }
+    });
+        
+        // AUDIO PROGRESS BAR SOUND 18
+
+    var myAudio18 = document.getElementById('player-18'); // get the audio data from the image-audio box
+    console.log(myAudio18)
+    myAudio18.addEventListener('progress18', function() {
+    var duration18 =  myAudio18.duration;
+    if (duration18 > 0) {
+      for (var i = 0; i < myAudio18.buffered18.length; i++) {
+            if (myAudio18.buffered18.start(myAudio18.buffered18.length - 1 - i) < myAudio18.currentTime) {
+                document.getElementById("buffered-amount18").style.width = (myAudio18.buffered18.end(myAudio18.buffered18.length - 1 - i) / duration18) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio18.addEventListener('timeupdate', function() {
+    var duration18 =  myAudio18.duration;
+    if (duration18 > 0) {
+      document.getElementById('progress-amount18').style.width = ((myAudio18.currentTime / duration18)*100) + "%";
+    }
+    });
+        
+    // AUDIO PROGRESS BAR SOUND 19
+
+    var myAudio19 = document.getElementById('player-19'); // get the audio data from the image-audio box
+    console.log(myAudio19)
+    myAudio19.addEventListener('progress19', function() {
+    var duration19 =  myAudio19.duration;
+    if (duration19 > 0) {
+      for (var i = 0; i < myAudio19.buffered19.length; i++) {
+            if (myAudio19.buffered19.start(myAudio19.buffered19.length - 1 - i) < myAudio19.currentTime) {
+                document.getElementById("buffered-amount19").style.width = (myAudio19.buffered19.end(myAudio19.buffered19.length - 1 - i) / duration19) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio19.addEventListener('timeupdate', function() {
+    var duration19 =  myAudio19.duration;
+    if (duration19 > 0) {
+      document.getElementById('progress-amount19').style.width = ((myAudio19.currentTime / duration19)*100) + "%";
+    }
+    });  
+        
+    // AUDIO PROGRESS BAR SOUND 20
+
+    var myAudio20 = document.getElementById('player-20'); // get the audio data from the image-audio box
+    console.log(myAudio20)
+    myAudio20.addEventListener('progress20', function() {
+    var duration20 =  myAudio20.duration;
+    if (duration20 > 0) {
+      for (var i = 0; i < myAudio20.buffered20.length; i++) {
+            if (myAudio20.buffered20.start(myAudio20.buffered20.length - 1 - i) < myAudio20.currentTime) {
+                document.getElementById("buffered-amount20").style.width = (myAudio20.buffered20.end(myAudio20.buffered20.length - 1 - i) / duration20) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio20.addEventListener('timeupdate', function() {
+    var duration20 =  myAudio20.duration;
+    if (duration20 > 0) {
+      document.getElementById('progress-amount20').style.width = ((myAudio20.currentTime / duration20)*100) + "%";
+    }
+    });  
+        
+    // AUDIO PROGRESS BAR SOUND 21
+
+    var myAudio21 = document.getElementById('player-21'); // get the audio data from the image-audio box
+    console.log(myAudio21)
+    myAudio21.addEventListener('progress21', function() {
+    var duration21 =  myAudio21.duration;
+    if (duration21 > 0) {
+      for (var i = 0; i < myAudio21.buffered21.length; i++) {
+            if (myAudio21.buffered21.start(myAudio21.buffered21.length - 1 - i) < myAudio21.currentTime) {
+                document.getElementById("buffered-amount21").style.width = (myAudio21.buffered21.end(myAudio21.buffered21.length - 1 - i) / duration21) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio21.addEventListener('timeupdate', function() {
+    var duration21 =  myAudio21.duration;
+    if (duration21 > 0) {
+      document.getElementById('progress-amount21').style.width = ((myAudio21.currentTime / duration21)*100) + "%";
+    }
+    });    
+    
+    // AUDIO PROGRESS BAR SOUND 22
+
+    var myAudio22 = document.getElementById('player-22'); // get the audio data from the image-audio box
+    console.log(myAudio22)
+    myAudio22.addEventListener('progress22', function() {
+    var duration22 =  myAudio22.duration;
+    if (duration22 > 0) {
+      for (var i = 0; i < myAudio22.buffered22.length; i++) {
+            if (myAudio22.buffered22.start(myAudio22.buffered22.length - 1 - i) < myAudio22.currentTime) {
+                document.getElementById("buffered-amount22").style.width = (myAudio22.buffered22.end(myAudio22.buffered22.length - 1 - i) / duration22) * 100 + "%";
+                break;
+            }
+        }
+    }
+    });
+    
+    myAudio22.addEventListener('timeupdate', function() {
+    var duration22 =  myAudio22.duration;
+    if (duration22 > 0) {
+      document.getElementById('progress-amount22').style.width = ((myAudio22.currentTime / duration22)*100) + "%";
+    }
+    });  
+        
     }
     
     // AUDIO TEXT LAYER EFFECTS
